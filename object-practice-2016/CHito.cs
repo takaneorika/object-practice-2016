53d35d558089fe7330e9d78410967f86012ba113
YuTanaka <YRK00337@nifty.com>
2017-02-07 14:30:22 +0900

    CHitoを追加
    - 第1段階のCHitoを実装

---
+++ object-practice-2016/CHito.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_practice_2016
+{
+    class CHito
+    {
+        /** 人の人数*/
+        private int count = 0;
+        /** 自分の名前*/
+        private string name = "";
+
+        /** コンストラクター*/
+        CHito(string nm)
+        {
+            name = nm;
+            count++;
+        }
+
+        /** 人数を返す*/
+        public int getCount()
+        {
+            return count;
+        }
+
+        /** 挨拶*/
+        public string hello()
+        {
+            return "私は" + name + "です。";
+        }
+
+        /** 行動*/
+        public string action()
+        {
+            return "テクテク";
+        }
+    }
+}
